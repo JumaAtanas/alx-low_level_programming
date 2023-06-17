3e122fad928ea57a3d2ee0d3f33f77634e7026a8
JumaAtanas <zacatanas@gmail.com>
2023-06-17 08:53:51 -0700

    task 5

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,8 @@
 #include  <stdio.h>
 /**
-  * main -C program that prints exactly "Programming is like building a multilingual puzzle
-  * Return: 0
-  */
+  * main -C program*/
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle"
-);
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
