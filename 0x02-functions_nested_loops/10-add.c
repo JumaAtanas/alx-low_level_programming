faba0c618b06462a5ce2592cdab50faa725e5eb0
JumaAtanas <zacatanas@gmail.com>
2023-06-20 12:41:15 -0700

    Task for nested object

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * add - Add two integers
+ * @x: int type number
+ * @y: int type number
+ * Return: result of addition
+ */
+int main()
+{
+	int add(int x, int y)
+	return (x + y);
+}
