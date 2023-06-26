c8ec43ace12efe1be58674ff160e6bf4ea144421
JumaAtanas <zacatanas@gmail.com>
2023-06-26 13:10:21 -0700

    Task 1

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,8 @@
+#include <main.h>
+/**
+ *comments
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,12 @@
+int _putchar(char c);
+int reset_to_98(int c);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+void _puts(char *str);
+void print_rev(char *s);
+void rev_string(char *s);
+void pits2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_starcpy(char *dest, char *src);
+char _atoi(char *s);
