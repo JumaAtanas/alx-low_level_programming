c31df6af93faa3216b84ba5b315c5c180d1d2154
JumaAtanas <zacatanas@gmail.com>
2023-06-26 13:59:03 -0700

    task 2

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * swap_int - Swwap the values of the two integers
+ *a@the first int to be swapped
+ @b the second it to be swappe
+ *
+ * Return: nothing
+ */
+void swap_int(int *a, int *b)
+{
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
